6fbba27847bf24f5e38da2acd8a719e06315d526
hans <hans.han@crointech.com>
2024-05-10 19:08:58 +0800

    w

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import dayjs from 'dayjs'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const toSlashDateString = (date: number | Date) => {
+  return dayjs(date).locale('zh-cn').format('YYYY年M月D日 dddd HH:mm:ss')
 }
